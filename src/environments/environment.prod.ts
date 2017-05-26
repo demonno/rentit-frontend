4d67e71cb82354ff1e84dde2dcd9bd4b34a0c574
Demur Nodia <demur.nodia@gmail.com>
2017-05-26 12:37:04 +0300

    Plans merge

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  RENTIT_BASE_URL: "http://193.40.11.150:8080",
+  RENTIT_BASE_URL: "http://193.40.11.150",
   MAINTENANCE_BASE_URL: "http://193.40.11.150:8090"
 };
